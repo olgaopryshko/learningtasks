0962091acfd978aef24d961407ca7a838bd149dc
olga opryshko <40028108+olgaopryshko@users.noreply.github.com>
2020-05-07 17:46:42 +0200

    xml files and menu changes

--- src/sportgames/team/TeamList.java
+++ src/sportgames/team/TeamList.java
@@ -41,33 +41,19 @@ public class TeamList {
         }
     }
 
-    private List<Team> readTeamsFromFile(String filePath) {
+    public void loadTeamsFromFile(String sportName, String filePath) {
         List<String> lines = FileRW.readFile(filePath);
-        List<Team> result = new ArrayList<>();
+        List<Team> teams = new ArrayList<>();
         for (String line : lines) {
             Team team = new Team(line);
-            result.add(team);
+            teams.add(team);
         }
-        return result;
-    }
-
-    public void readFootballTeamsFromFile(String filePath) {
-        List<Team> teams = readTeamsFromFile(filePath);
-        listFootballTeams.addAll(teams);
-    }
-
-    public void readHockeyTeamsFromFile(String filePath) {
-        List<Team> teams = readTeamsFromFile(filePath);
-        listHockeyTeams.addAll(teams);
-    }
-
-    public void readBasketballTeamsFromFile(String filePath) {
-        List<Team> teams = readTeamsFromFile(filePath);
-        listBasketballTeams.addAll(teams);
+        mapCollections.get(sportName).addAll(teams);
     }
 
 
-    private void saveTeamsToFile(String filePath, List<Team> teams) {
+    public void saveTeamsToFile(String sportName, String filePath) {
+        List<Team> teams = mapCollections.get(sportName);
         List<String> lines = new ArrayList<>();
         for (Team team : teams) {
             lines.add(team.getName());
@@ -75,52 +61,16 @@ public class TeamList {
         FileRW.writeFile(filePath, lines);
     }
 
-    public void saveFootballTeamsToFile(String filePath) {
-        saveTeamsToFile(filePath, getListFootballTeams());
+    public void addTeam(String sportName, Team team) {
+        mapCollections.get(sportName).add(team);
     }
 
-    public void saveBasketballTeamsToFile(String filePath) {
-        saveTeamsToFile(filePath, getListBasketballTeams());
+    public void removeTeam(String sportName, int index) {
+        mapCollections.get(sportName).remove(index);
     }
 
-    public void saveHockeyTeamsToFile(String filePath) {
-        saveTeamsToFile(filePath, getListHockeyTeams());
-    }
-
-    public void addFootballTeam(Team team) {
-        listFootballTeams.add(team);
-    }
-
-    public void removeFootballTeam(int index) {
-        listFootballTeams.remove(index);
-    }
-
-    public void addBasketballTeam(Team team) {
-        listBasketballTeams.add(team);
-    }
-
-    public void removeBasketballTeam(int index) {
-        listBasketballTeams.remove(index);
-    }
-
-    public void addHockeyTeam(Team team) {
-        listHockeyTeams.add(team);
-    }
-
-    public void removeHockeyTeam(int index) {
-        listHockeyTeams.remove(index);
-    }
-
-    public List<Team> getListFootballTeams() {
-        return listFootballTeams;
-    }
-
-    public List<Team> getListBasketballTeams() {
-        return listBasketballTeams;
-    }
-
-    public List<Team> getListHockeyTeams() {
-        return listHockeyTeams;
+    public List<Team> getTeamList(String sportName) {
+        return mapCollections.get(sportName);
     }
 
 
